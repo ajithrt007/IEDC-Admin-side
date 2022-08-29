eabff30b8d7bc520630504676df4538fef498722
ajithrt007 <ajithrt170@gmail.com>
2022-08-29 10:47:31 +0530

    preloader updation

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -1,4 +1,9 @@
+var scroll_y;
 var loader = document.querySelector(".page-loader");
+window.addEventListener("scroll", function(event) {  
+    var scroll_y = this.scrollY;
+    loader.style.top = scroll_y + 'px';
+});
 body.style.overflowY = "hidden";
 window.addEventListener("load",function(){
     loader.style.display = "none";
